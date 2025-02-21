4b6e244e7c005df77633b4500ad7d3756ad40305
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-21 17:31:52 -0500

    Create LocalDbService for conect sqlite

---
+++ SQLiteCrud_MAUI/LocalDbService.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace SQLiteCrud_MAUI
+{
+    public class LocalDbService
+    {
+        private const string DB_NAME = "sqlitecrud_db.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDbService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+        }
+    }
+}
