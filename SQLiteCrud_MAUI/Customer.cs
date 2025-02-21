406c8ffd6c271fcda44fde65c15694f6bf63f0de
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-21 17:35:39 -0500

    Create Customer model

---
+++ SQLiteCrud_MAUI/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SQLiteCrud_MAUI
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = default!;
+        public string Mobile { get; set; } = default!;
+        public string Email { get; set; } = default!;
+
+    }
+}
