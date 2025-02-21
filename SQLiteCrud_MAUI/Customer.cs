fa7e62ddcbcd1987d2491d806640f46594ca7f0f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-21 17:41:00 -0500

    Add dataAnnotations in model

--- SQLiteCrud_MAUI/Customer.cs
+++ SQLiteCrud_MAUI/Customer.cs
@@ -1,10 +1,25 @@
-﻿namespace SQLiteCrud_MAUI
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations.Schema;
+using ColumnAttribute = SQLite.ColumnAttribute;
+using TableAttribute = System.ComponentModel.DataAnnotations.Schema.TableAttribute;
+
+namespace SQLiteCrud_MAUI
 {
+    [Table("customer")]
     public class Customer
     {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("customer_name")]
         public string CustomerName { get; set; } = default!;
+
+        [Column("mobile")]
         public string Mobile { get; set; } = default!;
+
+        [Column("email")]
         public string Email { get; set; } = default!;
 
     }
